48e59e9b10f07f7e2c1db6670a7d0f5ed5946ce3
katryo <katoryo55@gmail.com>
2014-01-03 16:39:44 +0900

    add_thanks

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ remote_mysql
 ============
 
 connect and fetch data from the MySQL in the server without ssh command with paramiko and MySQLdb
+
+
+Thanks to http://d.hatena.ne.jp/yuheiomori0718/20121031/1351692579
--- paramiko_sample.py
+++ paramiko_sample.py
@@ -16,11 +16,6 @@ if __name__ == '__main__':
         print('connected!')
         sftp_connection = client.open_sftp()
 
-        # ホームディレクトリのファイル一覧をprint
-        files = sftp_connection.listdir()
-        for remote_file in files:
-            print(remote_file)
-
         # ファイルを取得
         # sftp_connection.get('test.py', 'test.py')
 
