c72afd615c17672393a85e0285744caaf33b41ca
katryo <katoryo55@gmail.com>
2014-01-03 17:17:40 +0900

    実験した

--- sql_commands.py
+++ sql_commands.py
@@ -1,3 +1,3 @@
 # coding=utf-8
 sample_1 = 'select * from all_hyponymy where hyponym = "カードゲーム" limit 5;'
-sample_2 = 'select * from all_hyponymy where hypernym = "格闘技" limit 5;'
+sample_2 = 'select * from all_hyponymy where hypernym = "経験" limit 5;'
